0c96692381cd55f738f54ed28ae0994681711553
Subham Narayan <subhamnarayan11144@gmail.com>
2025-08-03 22:05:37 +0530

    update

--- 2DARRAY/TwoDimarrayAddition.java
+++ 2DARRAY/TwoDimarrayAddition.java
@@ -7,11 +7,12 @@ class TwoDarray{
     int c[][]=new int[3][3];
 
     void getdata(){
+        Scanner sc = new Scanner(System.in);
         for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                System.out.print("ENTER THE NUMBER :");
-                Scanner sc = new Scanner(System.in);
+            for(int j=0;j<3;j++){  
+                System.out.print("ENTER THE NUMBER FOR 1ST MATRIX:");
                 a[i][j]=sc.nextInt();
+                System.out.print("ENTER THE NUMBER FOR 2ND MATRIX:");
                 b[i][j]=sc.nextInt();
             }
         }
@@ -20,9 +21,9 @@ class TwoDarray{
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
 
-                System.out.print(c[i][j]=a[i][j]+b[i][j] + "  ");    
+                c[i][j]=a[i][j]+b[i][j] ;    
             }
-            System.out.println();
+            
             }
 
     }
