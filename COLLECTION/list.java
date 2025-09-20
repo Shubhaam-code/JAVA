cc65647be0bbfab7688dd313400f19317a750dcc
Subham Narayan <subhamnarayan11144@gmail.com>
2025-09-20 22:41:21 +0530

    update

---
+++ COLLECTION/list.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class list
+{
+    public static void main(String []args)
+    {
+        ArrayList l = new ArrayList();
+        l.add(10);
+        System.out.println(l);
+    }
+}
